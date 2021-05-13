132f86b467d97149eeee20f32176a3d2fe516049
Akinori Toma <akinoritoma@appletoma.home>
2021-05-12 20:31:55 -0400

    make TicketCard model

--- kanban_challenge/models.py
+++ kanban_challenge/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 # Create your models here.
+
+class TicketCard (models.Model):
+  status = models.IntegerField(default=1)
+  post_person = models.CharField("PostPerson", max_length=20, default="John Due")
+  content = models.TextField("Contents") 
+  created = models.DateTimeField("Date", default=timezone.now)
+
+  def __str__(self):
+    return self.status
--- kanban_challenge/views.py
+++ kanban_challenge/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
   template_name = "index.html"
